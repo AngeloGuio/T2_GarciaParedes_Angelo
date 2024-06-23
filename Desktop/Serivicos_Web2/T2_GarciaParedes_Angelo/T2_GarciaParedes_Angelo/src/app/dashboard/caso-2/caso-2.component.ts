ca4f4d2dd84750f823f1d96bc5c6c8e5a69410d4
AngeloGarcia <guioangelo.garcia@gmail.com>
2024-06-22 21:28:16 -0500

    caso 2 primer commir

--- Desktop/Serivicos_Web2/T2_GarciaParedes_Angelo/T2_GarciaParedes_Angelo/src/app/dashboard/caso-2/caso-2.component.ts
+++ Desktop/Serivicos_Web2/T2_GarciaParedes_Angelo/T2_GarciaParedes_Angelo/src/app/dashboard/caso-2/caso-2.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-caso-2',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './caso-2.component.html',
   styleUrl: './caso-2.component.css'
 })
 export class Caso2Component {
+  numeroMaterias: number = 0;
+  costoMateria: number = 520;
+  totalMatricula: number | null = null;
+
+  calcularMatricula() {
+    let total = this.numeroMaterias * this.costoMateria;
+    if (this.numeroMaterias > 5) {
+      total = total * 0.90;  // Aplicar 10% de descuento
+    }
+    this.totalMatricula = total;
+  }
 
 }
