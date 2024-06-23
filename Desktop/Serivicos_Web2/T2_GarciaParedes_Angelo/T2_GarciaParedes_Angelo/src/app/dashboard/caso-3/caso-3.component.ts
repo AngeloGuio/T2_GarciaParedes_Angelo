5e3a5ea608b5e730c11a850e0aa222ea659247ec
AngeloGarcia <guioangelo.garcia@gmail.com>
2024-06-22 21:35:42 -0500

    caso 2 segundo commit

--- Desktop/Serivicos_Web2/T2_GarciaParedes_Angelo/T2_GarciaParedes_Angelo/src/app/dashboard/caso-3/caso-3.component.ts
+++ Desktop/Serivicos_Web2/T2_GarciaParedes_Angelo/T2_GarciaParedes_Angelo/src/app/dashboard/caso-3/caso-3.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-caso-3',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './caso-3.component.html',
   styleUrl: './caso-3.component.css'
 })
 export class Caso3Component {
+   tipoVehiculo: string = '';
+  tasaAlcohol: number | null = null;
+  resultado: string | null = null;
+
+  calcularResultado() {
+    const tasaMaxima = this.getTasaMaxima(this.tipoVehiculo);
+    if (this.tasaAlcohol !== null) {
+      if (this.tasaAlcohol <= tasaMaxima) {
+        this.resultado = 'Negativo';
+      } else {
+        this.resultado = 'Positivo';
+      }
+    }
+  }
+
+  getTasaMaxima(tipoVehiculo: string): number {
+    switch (tipoVehiculo) {
+      case 'C':
+      case 'A':
+      case 'M':
+        return 0.3;
+      case 'T':
+        return 0.5;
+      default:
+        return 0;
+    }
+  }
 
 }
